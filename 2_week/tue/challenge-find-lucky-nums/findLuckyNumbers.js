60e1e3103ee4197339a530175aa0047acb66ff76
Chase Morgan <chasemorgan15@yahoo.com>
2023-04-12 08:51:17 -0400

    added labs

---
+++ 2_week/tue/challenge-find-lucky-nums/findLuckyNumbers.js
@@ -0,0 +1,22 @@
+// Write your code below this line.
+
+// Given a number, n, return an array containing n unique random numbers between 1-10, inclusive. (That is, do not repeat any numbers in the returned list.)
+
+// You can trust that this function will never be called with n < 0 or n > 10.
+
+const uniqueRandomNumber = n =>{
+    const arr = []
+
+
+
+
+}
+
+
+
+/**
+ given a number for a param and returning an array containg unique random numbers between 1 and 10 
+
+
+
+ */
